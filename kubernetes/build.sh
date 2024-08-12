300b26557d1da42b3d9206406e268d9c37cb9f25
Connor Black <connorjosephblack@gmail.com>
2024-08-12 16:23:30 -0400

    feat: add support for building and deploying to kubernetes
    Adds a new helm chart for deploying the Sellie database to a Kubernetes cluster. Includes support for building the Docker image and creating a docker registry secret.

--- kubernetes/build.sh
+++ kubernetes/build.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 set -euo pipefail
+
+BUILD_IMAGE=false
+CREATE_DOCKER_SECRET=false
+HELM_RELEASE_NAME="sellie-db"
+VALUES_FILE="/Users/connorblack/GitHub/sellie-db/patroni/kubernetes/sellie-db/values.yaml"
+HELM_PATH="/Users/connorblack/GitHub/sellie-db/patroni/kubernetes/sellie-db"
+DOCKERFILE_PATH="/Users/connorblack/GitHub/sellie-db/patroni/kubernetes/Dockerfile.citus.dev"
+DOCKERHUB_REPO="connorblack/patroni-citus-k8s"
+DOCKERHUB_USERNAME="connorblack"
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+  --build) BUILD_IMAGE=true ;;
+  --create-secret) CREATE_DOCKER_SECRET=true ;;
+  *)
+    echo "Unknown parameter passed: $1"
+    exit 1
+    ;;
+  esac
+  shift
+done
+
 if [ -f .env ]; then
   source .env
 else
@@ -8,19 +31,24 @@ else
   exit 1
 fi
 
-# docker buildx build --builder=cloud-connorblack-default -t connorblack/patroni --platform=linux/amd64,linux/arm64 --push .
+if [ "$BUILD_IMAGE" = true ]; then
+  docker buildx build --builder=cloud-connorblack-default -f $DOCKERFILE_PATH -t $DOCKERHUB_REPO --platform=linux/amd64,linux/arm64 --push .
+fi
 
-kubectl delete -f citus_k8s.yaml --ignore-not-found
+if [ "$CREATE_DOCKER_SECRET" = true ]; then
+  kubectl create secret docker-registry docker-regcred \
+    --docker-username=$DOCKERHUB_USERNAME \
+    --docker-password="$DOCKER_PASSWORD"
+fi
 
-docker buildx build --builder=cloud-connorblack-default -f Dockerfile.citus.dev -t connorblack/patroni-citus-k8s --platform=linux/amd64,linux/arm64 --push .
+helm template $HELM_RELEASE_NAME $HELM_PATH -f $VALUES_FILE --debug
 
-# kubectl create secret docker-registry docker-regcred \
-#   --docker-server=docker.pkg.github.com \
-#   --docker-username=connorblack \
-#   --docker-password="$DOCKER_PASSWORD"
+# if release exists, run helm upgrade
+if helm list -n default | grep $HELM_RELEASE_NAME; then
+  helm upgrade $HELM_RELEASE_NAME $HELM_PATH -f $VALUES_FILE --atomic --debug
+else
+  helm install $HELM_RELEASE_NAME $HELM_PATH -f $VALUES_FILE --atomic --debug
+fi
 
-kubectl apply -f citus_k8s.yaml
-
-# Port forward to allow local connections
-# kubectl port-forward pod/citusdemo-0-0 5432:5432 &
-echo "Port forwarding started. You can now connect to the database on localhost:5432"
+# port forward 5432 to localhost
+kubectl port-forward pods/selliecluster-0-0 8008:8008 5432:5432 -n default
